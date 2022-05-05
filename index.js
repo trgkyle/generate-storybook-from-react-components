4dbf48d10b70af13ccc76933024b291f4324e1f6
Nguyễn Xuân Trường (Kyle) <truongondaihoc@gmail.com>
2022-05-05 15:36:30 +0000

    refactor render

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const { promisify } = require('util');
 const { join } = require('path');
 const readFile = promisify(require('fs').readFile);
 const writeFile = promisify(require('fs').writeFile);
-const Path = require("path");
-const FS   = require("fs");
-const Files  = [];
+const path = require("path");
+const fs = require("fs");
+const files = [];
 
 function ThroughDirectory(Directory) {
-    FS.readdirSync(Directory).forEach(File => {
-        const Absolute = Path.join(Directory, File);
-        if (FS.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
-        else return Files.push(Absolute);
-    });
+	fs.readdirSync(Directory).forEach(File => {
+		const absolute = path.join(Directory, File);
+		if (fs.statSync(absolute).isDirectory()) return ThroughDirectory(absolute);
+		else return files.push(absolute);
+	});
 }
 async function runner(config, rootPath) {
 	let content = `import React from 'react';
@@ -28,16 +28,16 @@ storiesOf('___tpl_name___', module)
 	const dirnames = __dirname + config.path;
 	console.log("Scan all files components in ", dirnames);
 	ThroughDirectory(dirnames);
-	console.log(Files);
-	const pathRelative = config.path;
+	console.log(files);
+	const subPathRelative = config.path;
 	if (config.path.substr(0, 2) !== '..') {
 		config.path = `..${config.path}`;
 	}
 
-	await Promise.all(Files.map(async(dir)=> {
-		const a = dir.split(pathRelative)[1].split('/');
-		a.splice(-1);
-		const path = config.path +  a.join('/');
+	await Promise.all(files.map(async (dir) => {
+		const relativePath = dir.split(subPathRelative)[1].split('/');
+		relativePath.splice(-1);
+		const path = config.path + relativePath.join('/');
 		const pathNameSplit = dir.split('/');
 		const name = pathNameSplit[pathNameSplit.length - 1].split('.js')[0];
 		content = content.replace(/___component_path___/g, path);
